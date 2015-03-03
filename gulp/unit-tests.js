f49ad9ec726e1856d210ed033496312c5b8135ff
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-03-03 15:58:07 -0600

    test:wiredep task now properly injects bower deps into karma.conf

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -1,31 +1,81 @@
 'use strict';
 
 var gulp = require('gulp');
+var path = require('path');
+var karma = require('karma').server;
+var wiredep = require('wiredep').stream;
 
-var $ = require('gulp-load-plugins')();
-
-var wiredep = require('wiredep');
-
-gulp.task('test', function() {
-  var bowerDeps = wiredep({
-    directory: 'bower_components',
-    exclude: [/bootstrap-sass-official/, /\/bootstrap.js/, /bootstrap.css/, /jquery.js/],
-    dependencies: true,
-    devDependencies: true
-  });
-
-  var testFiles = bowerDeps.js.concat([
-    'src/{app,components}/**/*.js',
-    'test/unit/**/*.js'
-  ]);
-
-  return gulp.src(testFiles)
-    .pipe($.karma({
-      configFile: 'test/karma.conf.js',
-      action: 'run'
-    }))
-    .on('error', function(err) {
-      // Make sure failed tests cause gulp to exit non-zero
-      throw err;
-    });
+/**
+ * Run test once and exit
+ */
+gulp.task('test', ['test:wiredep'], function (done) {
+  karma.start({
+    configFile: path.resolve('test/karma.conf.js'),
+    singleRun: true
+  }, done);
 });
+
+/**
+ * Watch for file changes and re-run tests on each change
+ */
+gulp.task('test:watch', ['test:wiredep'], function (done) {
+  karma.start({
+    configFile: path.resolve('test/karma.conf.js')
+  }, done);
+});
+
+// inject bower components into karma.conf
+gulp.task('test:wiredep', function () {
+  return gulp.src('test/karma.conf.js')
+    .pipe(wiredep({
+      directory: 'bower_components',
+      exclude: [/bootstrap-sass-official/, /\/bootstrap.js/, /bootstrap.css/, /jquery.js/],
+      devDependencies: true,
+      ignorePath: '../',
+      fileTypes: {
+        js: {
+          block: /(([ \t]*)\/\/\s*bower:*(\S*))(\n|\r|.)*?(\/\/\s*endbower)/gi,
+          detect: {
+            js: /".*\.js"/gi
+          },
+          replace: {
+            js: '"{{filePath}}",'
+          }
+        }
+      }
+    }))
+    .pipe(gulp.dest('test'));
+});
+
+//var gulp = require('gulp');
+//
+//var $ = require('gulp-load-plugins')();
+//
+//var wiredep = require('wiredep');
+//
+//gulp.task('test', function() {
+//  var bowerDeps = wiredep({
+//    directory: 'bower_components',
+//    exclude: [/bootstrap-sass-official/, /\/bootstrap.js/, /bootstrap.css/, /jquery.js/],
+//    dependencies: true,
+//    devDependencies: true
+//  });
+//
+//  var testFiles = bowerDeps.js.concat([
+//    'src/{app,components}/**/*.js',
+//    'test/unit/**/*.js'
+//  ]);
+//
+//  return gulp.src(testFiles)
+//    .pipe($.karma({
+//      configFile: 'test/karma.conf.js',
+//      action: 'run'
+//    }))
+//    .on('error', function(err) {
+//      // Make sure failed tests cause gulp to exit non-zero
+//      throw err;
+//    });
+//});
+{
+
+}
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,14 +1,24 @@
 'use strict';
-
 module.exports = function(config) {
-
   config.set({
-    basePath : '..', //!\\ Ignored through gulp-karma //!\\
-
-    files : [ //!\\ Ignored through gulp-karma //!\\
-      'src/bower_components/angular/angular.js',
-      'src/bower_components/angular/angular-route.js',
-      'src/bower_components/angular-mocks/angular-mocks.js',
+    basePath : '..',
+    files : [
+      // bower:js
+      "bower_components/angular/angular.js",
+      "bower_components/angular-sanitize/angular-sanitize.js",
+      "bower_components/angular-ui-router/release/angular-ui-router.js",
+      "bower_components/ui-router-extras/release/ct-ui-router-extras.js",
+      "bower_components/angular-ui-grid/ui-grid.js",
+      "bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
+      "bower_components/angular-translate/angular-translate.js",
+      "bower_components/angular-dialog-service/dist/dialogs.min.js",
+      "bower_components/angular-dialog-service/dist/dialogs-default-translations.min.js",
+      "bower_components/lodash/lodash.js",
+      "bower_components/angular-resource/angular-resource.js",
+      "bower_components/angular-timeago/src/timeAgo.js",
+      "bower_components/angular-agility/dist/angular-agility.min.js",
+      "bower_components/angular-mocks/angular-mocks.js",
+      // endbower
       'src/{app,components}/** /*.js',
       'test/unit/** /*.js'
     ],
